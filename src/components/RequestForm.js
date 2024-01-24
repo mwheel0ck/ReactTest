7aca2bbbb29f6bf2262c9860777f741a1e2ae505
Michael <mike@sokpuppet.org>
2024-01-23 19:09:44 -0600

    disconnected GPS

--- src/components/RequestForm.js
+++ src/components/RequestForm.js
@@ -13,11 +13,11 @@ export default function RequestForm() {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
     //let dailyAPIURL =
-    "https://api.openweathermap.org/data/2.5/forecast/daily?lat=" +
-      lat +
-      "&lon=" +
-      lon +
-      "&cnt=16&units=imperial&appid=2d41c22ae78b3bd082fd3f0eda60e983";
+    //"https://api.openweathermap.org/data/2.5/forecast/daily?lat=" +
+    // lat +
+    //  "&lon=" +
+    //  lon +
+    //  "&cnt=16&units=imperial&appid=2d41c22ae78b3bd082fd3f0eda60e983";
     //console.log(dailyAPIURL);
     //axios.get(dailyAPIURL).then((res2) => {
     //console.log(res2.data.list);
